a9b8c0a4660043993d580feaddc577014657a3b4
tshankar35 <63742827+tshankar35@users.noreply.github.com>
2022-04-02 17:19:56 +0530

    Add files via upload

---
+++ kresources/ktemplates/frontend/side_nav.php
@@ -0,0 +1,9 @@
+<div class="col-md-3">
+<p class="lead">Girlscart</p>
+   <div class="list-group">
+       <?php
+           //  <--Category -->
+                get_categories();
+       ?>
+    </div>
+ </div> 
